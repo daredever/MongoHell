9279be4a4072c06698db79165eb444a551c1ebcd
Roman <daredever@gmail.com>
2020-02-29 15:53:18 +0300

    edit profiler

--- src/Repository/Profiler.cs
+++ src/Repository/Profiler.cs
@@ -8,22 +8,24 @@ namespace Repository
 		public Profiler(string stage)
 		{
 			_stage = stage;
-			_stopwatch = Stopwatch.StartNew();
+			_stopWatch = Stopwatch.StartNew();
 		}
 
 		public void Dispose()
 		{
-			_stopwatch.Stop();
-			var nanosecPerTick = (1000L * 1000L * 1000L) / Stopwatch.Frequency;
-			var nanosecToMillisec = 1000L * 1000L;
-			var timeInMillisec = Math.Round((decimal) (_stopwatch.ElapsedTicks * nanosecPerTick) / nanosecToMillisec, 4);
+			_stopWatch.Stop();
+			
+			var timeInMillisec = Math.Round((decimal) (_stopWatch.ElapsedTicks * _nanosecPerTick) / _nanosecToMillisec, 4);
 
 			// write data with _stage and timeInMillisec
 		}
 
 		public static Profiler GetProfiler(string stage) => new Profiler(stage);
 
-		private readonly Stopwatch _stopwatch;
+		private static long _nanosecPerTick = 1000000000L / Stopwatch.Frequency;
+		private static long _nanosecToMillisec = 1000000L;
+		
+		private readonly Stopwatch _stopWatch;
 		private readonly string _stage;
 	}
 }
