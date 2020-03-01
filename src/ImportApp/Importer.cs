7859d9e86b74ce1051a67500a52cea9f90c658d9
Roman <daredever@gmail.com>
2020-03-01 11:53:03 +0300

    update code for live coding

---
+++ src/ImportApp/Importer.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading.Tasks;
+
+namespace ImportApp
+{
+	public class Importer
+	{
+		public Importer()
+		{
+			
+		}
+
+		public Task ImportAsync()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/ImportApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ImportApp
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var importer = new Importer();
+			await importer.ImportAsync();
+		}
+	}
+}
