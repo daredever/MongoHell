970876dac950cb9e575e90f74d6c47a23e19d045
Roman Shcherbakov <daredever@gmail.com>
2020-03-05 14:53:59 +0300

    fix ext

---
+++ src/ImportApp/Extensions/EnumerableExtentions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImportApp.Extensions
+{
+	public static class EnumerableExtentions
+	{
+		/// <summary>
+		/// Performs the specified delegate on each element of a collection. 
+		/// For each asynchronously.
+		/// </summary>
+		/// <param name="source">Source collection</param>
+		/// <param name="partitionCount">Degree of parallelism</param>
+		/// <param name="function">Delegate</param>
+		/// <typeparam name="T">Input type</typeparam>
+		/// <returns>Task</returns>
+		public static Task ForEachAsync<T>(this IEnumerable<T> source, int partitionCount, Func<T, Task> function)
+		{
+			var partitions = Partitioner.Create(source).GetPartitions(partitionCount);
+			var tasks = partitions.Select(partition =>
+				Task.Run(async () =>
+				{
+					using (partition)
+					{
+						while (partition.MoveNext())
+						{
+							await function(partition.Current).ConfigureAwait(false);
+						}
+					}
+				}));
+
+			return Task.WhenAll(tasks);
+		}
+	}
+}
