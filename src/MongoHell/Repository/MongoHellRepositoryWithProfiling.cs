a29696c41a4121d153a6667a8858301a486d14b6
Roman <daredever@gmail.com>
2020-03-09 13:12:49 +0300

    fix profiler

--- src/MongoHell/Repository/MongoHellRepositoryWithProfiling.cs
+++ src/MongoHell/Repository/MongoHellRepositoryWithProfiling.cs
@@ -12,21 +12,21 @@ namespace MongoHell.Repository
 
 		public override Task AddOrUpdateItemAsync<T>(T item, string collectionName)
 		{
-			var stage = $"{nameof(AddOrUpdateItemAsync)}({typeof(T).Name}, \"{collectionName}\")";
+			var stage = $"{nameof(AddOrUpdateItemAsync)}({typeof(T).Name}, \"{collectionName})\"";
 			using var profiler = Profiler.GetProfiler(stage);
 			return base.AddOrUpdateItemAsync(item, collectionName);
 		}
 
 		public override Task<T> GetItemAsync<T>(string id, string collectionName)
 		{
-			var stage = $"{nameof(GetItemAsync)}(\"{id}\", \"{collectionName})\"";
+			var stage = $"{nameof(GetItemAsync)}(\"{id}\", \"{collectionName}\")";
 			using var profiler = Profiler.GetProfiler(stage);
 			return base.GetItemAsync<T>(id, collectionName);
 		}
 
 		public override Task DeleteItemAsync<T>(string id, string collectionName)
 		{
-			var stage = $"{nameof(DeleteItemAsync)}(\"{id}\", \"{collectionName})\"";
+			var stage = $"{nameof(DeleteItemAsync)}(\"{id}\", \"{collectionName}\")";
 			using var profiler = Profiler.GetProfiler(stage);
 			return base.DeleteItemAsync<T>(id, collectionName);
 		}
