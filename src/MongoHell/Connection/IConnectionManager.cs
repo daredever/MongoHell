cd254cf1ee020d9e346434a6a55743fa65bc3338
Roman Shcherbakov <daredever@gmail.com>
2020-03-26 15:02:07 +0300

    fix comments

--- src/MongoHell/Connection/IConnectionManager.cs
+++ src/MongoHell/Connection/IConnectionManager.cs
@@ -17,14 +17,12 @@
 		/// Returns database by name.
 		/// </summary>
 		/// <param name="dbName">Database name.</param>
-		/// <exception cref="InvalidOperationException">Thrown if manager was not initialized.</exception>
 		/// <returns>Database</returns>
 		IMongoDatabase GetDatabase(string dbName);
 
 		/// <summary>
 		/// Opens a new client session. 
 		/// </summary>
-		/// <exception cref="InvalidOperationException">Thrown if manager was not initialized.</exception>
 		/// <returns>New client session.</returns>
 		IClientSessionHandle GetNewSession();
 	}
